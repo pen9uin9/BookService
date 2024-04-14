05c06f21c3d930734acc89bb501c6dd0fcb2a093
hyun <hyunguin9@gmail.com>
2024-04-14 21:52:25 +0900

    ch7 내용과 welcome.java 어래이 리스트 추가해주기

---
+++ HomeWork/src/homework/CartItem.java
@@ -0,0 +1,56 @@
+package homework;
+
+public class CartItem {
+	private String[] itemBook = new String[7];
+	private String bookID;
+	private int quantity;
+	private int totalPrice;
+	
+	public CartItem() {
+		
+	}
+	
+	public CartItem(String[]book) {
+		this.setItemBook(book);
+		this.setBookID(book[0]);
+		this.setQuantity(1);
+		updateTotalPrice();
+	}
+
+	public String[] getItemBook() {
+		return itemBook;
+	}
+
+	public void setItemBook(String[] itemBook) {
+		this.itemBook = itemBook;
+	}
+
+	public String getBookID() {
+		return bookID;
+	}
+
+	public void setBookID(String bookID) {
+		this.bookID = bookID;
+		this.updateTotalPrice();
+	}
+
+	public int getQuantity() {
+		return quantity;
+	}
+
+	public void setQuantity(int quantity) {
+		this.quantity = quantity;
+		this.updateTotalPrice();
+	}
+
+	public int getTotalPrice() {
+		return totalPrice;
+	}
+
+	public void updateTotalPrice() {
+		totalPrice = Integer.parseInt(this.itemBook[2])* this.quantity;
+	}
+	
+	
+	
+}
